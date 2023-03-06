d47b23388020c336a01529ed90fd521ab917dd9e
Mehnaz <78849132+1mazin@users.noreply.github.com>
2023-03-06 16:33:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+var cors = require('cors');
+const stripe = require('stripe')('##yourstripesecretkey##');
+ 
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+ 
+app.post("/checkout", async (req, res) => {
+    console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item)=> {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
+ 
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+    });
+ 
+    res.send(JSON.stringify({
+        url: session.url
+    }));
+});
+ 
+app.listen(4000, () => console.log("Listening on port 4000!"));
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
